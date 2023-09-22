90ecb470da41a6015f0c417d72bca877f75b290b
Ivan Contreras <ivan.contreras@cablasociados.com>
2023-09-22 18:38:07 -0300

    Code commit

---
+++ Evaluacion_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Evaluacion_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
